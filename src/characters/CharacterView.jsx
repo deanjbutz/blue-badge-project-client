1cab74a7e0f0f982468d7b7de15322c913c6aa7e
Dean Butz <deanjbutz@gmail.com>
2021-12-15 11:26:13 -0500

    pulled styling and working on modals

--- src/characters/CharacterView.jsx
+++ src/characters/CharacterView.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Table, Button, Modal } from 'reactstrap';
+import { Table, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CharacterView = (props) => {
@@ -37,87 +37,122 @@ const CharacterView = (props) => {
     }
 
     return (
-        <div>
-        {/* <Modal isOpen={true}> */}
+        // <div>
+        <Modal isOpen={true} size="lg" style={{maxWidth: '90%', paddingTop: '5%'}}>
+            <ModalHeader toggle={function noRefCheck(){}}>
+                CharacterView
+            </ModalHeader>
 
-            <h3>CharacterView</h3>
-            <Button onClick={() => props.toggleViewCharacter()}>Close</Button>
+            <ModalBody>
+                <Table striped>
+                    <thead>
+                        <tr>
+                            <td>Race</td>
+                            <td>Class</td>
+                            <td>Background</td>
+                            <td>Level</td>
+                            <td>Strength</td>
+                            <td>Dexterity</td>
+                            <td>Constitution</td>
+                            <td>Intelligence</td>
+                            <td>Wisdom</td>
+                            <td>Charisma</td>
+                            <td>ClassSkill</td>
+                            <td>BackgroundTool</td>
+                            <td>RaceLanguage</td>
+                            <td>FightingStyle</td>
+                            <td>BackgroundSpeciality</td>
+                            <td>HitPoints</td>
+                            <td>KnownSpell</td>
+                            <td>Armor</td>
+                            <td>Weapon</td>
+                            <td>Tool</td>
+                            <td>Gender</td>
+                            <td>Height</td>
+                            <td>Weight</td>
+                            <td>Character Backstory</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{props.character.race}</td>
+                            <td>{props.character.chrClass}</td>
+                            <td>{props.character.background}</td>
+                            <td>{props.character.level}</td>
+                            <td>{props.character.strength}</td>
+                            <td>{props.character.dexterity}</td>
+                            <td>{props.character.constitution}</td>
+                            <td>{props.character.intelligence}</td>
+                            <td>{props.character.wisdom}</td>
+                            <td>{props.character.charisma}</td>
+                            <td>{props.character.classSkill}</td>
+                            <td>{props.character.backgroundTool}</td>
+                            <td>{props.character.raceLanguage}</td>
+                            <td>{props.character.fightingStyle}</td>
+                            <td>{props.character.backgroundSpeciality}</td>
+                            <td>{props.character.hitPoints}</td>
+                            <td>{props.character.knownSpell}</td>
+                            <td>{props.character.armor}</td>
+                            <td>{props.character.weapon}</td>
+                            <td>{props.character.tool}</td>
+                            <td>{props.character.gender}</td>
+                            <td>{props.character.height}</td>
+                            <td>{props.character.weight}</td>
+                            <td>{props.character.characterBackstory}</td>
+                            <td>{props.character.owner_id}</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </ModalBody>
 
-            <Table striped>
-                <thead>
-                    <tr>
-                        <td>Race</td>
-                        <td>Class</td>
-                        <td>Background</td>
-                        <td>Level</td>
-                        <td>Strength</td>
-                        <td>Dexterity</td>
-                        <td>Constitution</td>
-                        <td>Intelligence</td>
-                        <td>Wisdom</td>
-                        <td>Charisma</td>
-                        <td>ClassSkill</td>
-                        <td>BackgroundTool</td>
-                        <td>RaceLanguage</td>
-                        <td>FightingStyle</td>
-                        <td>BackgroundSpeciality</td>
-                        <td>HitPoints</td>
-                        <td>KnownSpell</td>
-                        <td>Armor</td>
-                        <td>Weapon</td>
-                        <td>Tool</td>
-                        <td>Gender</td>
-                        <td>Height</td>
-                        <td>Weight</td>
-                        <td>Character Backstory</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{props.character.race}</td>
-                        <td>{props.character.chrClass}</td>
-                        <td>{props.character.background}</td>
-                        <td>{props.character.level}</td>
-                        <td>{props.character.strength}</td>
-                        <td>{props.character.dexterity}</td>
-                        <td>{props.character.constitution}</td>
-                        <td>{props.character.intelligence}</td>
-                        <td>{props.character.wisdom}</td>
-                        <td>{props.character.charisma}</td>
-                        <td>{props.character.classSkill}</td>
-                        <td>{props.character.backgroundTool}</td>
-                        <td>{props.character.raceLanguage}</td>
-                        <td>{props.character.fightingStyle}</td>
-                        <td>{props.character.backgroundSpeciality}</td>
-                        <td>{props.character.hitPoints}</td>
-                        <td>{props.character.knownSpell}</td>
-                        <td>{props.character.armor}</td>
-                        <td>{props.character.weapon}</td>
-                        <td>{props.character.tool}</td>
-                        <td>{props.character.gender}</td>
-                        <td>{props.character.height}</td>
-                        <td>{props.character.weight}</td>
-                        <td>{props.character.characterBackstory}</td>
-                        <td>{props.character.owner_id}</td>
-                        {
-                            (props.token.includes('Bearer')) ?
-                            JSON.parse(window.atob(props.token.split('.')[1])).id === props.character.owner_id ?
-                                <>
+            <ModalFooter>
+            {
+                                (props.token.includes('Bearer')) ?
+                                JSON.parse(window.atob(props.token.split('.')[1])).id === props.character.owner_id ?
+                                    <>
+                                        <td>
+                                        <Button color="warning" onClick={() => props.toggleViewEditCharacter()}>Edit</Button>
+                                    </td>
                                     <td>
-                                    <Button color="warning" onClick={() => props.toggleViewEditCharacter()}>Edit</Button>
-                                </td>
-                                <td>
-                                    <Button color="danger" onClick={() => {deleteCharacter(props.character)}}>Delete</Button>
-                                </td>
-                                </>
+                                        <Button color="danger" onClick={() => {deleteCharacter(props.character)}}>Delete</Button>
+                                    </td>
+                                    </>
+                                    : null
                                 : null
-                            : null
-                        }
-                    </tr>
-                </tbody>
-            </Table>
-        {/* </Modal> */}
-        </div>
+                            }
+                <Button onClick={() => props.toggleViewCharacter()}>Close</Button>            
+            </ModalFooter>
+        </Modal>
+        // </div>
+
+
+
+  
+//   <Modal
+//   isOpen={true}
+//   >
+//     <ModalHeader toggle={function noRefCheck(){}}>
+//       Modal title
+//     </ModalHeader>
+//     <ModalBody>
+//       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+//     </ModalBody>
+//     <ModalFooter>
+//       <Button
+//         color="primary"
+//         onClick={function noRefCheck(){}}
+//       >
+//         Do Something
+//       </Button>
+//       {' '}
+//       <Button onClick={function noRefCheck(){}}>
+//         Cancel
+//       </Button>
+//     </ModalFooter>
+//   </Modal>
+
+
+
     )
 }
 
